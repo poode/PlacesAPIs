89a97b1b9707298c19466844b0e93a1ff845a8bd
poode <eng.abdul.azeem@gmail.com>
2020-04-05 18:42:54 +0200

    add route to add a city for any user

--- app/router/city.js
+++ app/router/city.js
@@ -2,12 +2,13 @@ const router = require('express-promise-router')();
 
 const { jwt } = require('../services/strategies');
 const { validate } = require('../middelwares/validator');
-// const { adminRole } = require('../middelwares/adminRole');
+const { adminRole } = require('../middelwares/adminRole');
 
 const { addCitySchema } = require('../RequestSchemaList/addCitySchema');
 
 const { self, addCity } = require('../controllers/City');
 
-router.post('/', jwt/*, adminRole*/, validate(addCitySchema), addCity.bind(self));
+router.post('/', jwt, adminRole, validate(addCitySchema), addCity.bind(self));
+router.post('/users', jwt/*, adminRole*/, validate(addCitySchema), addCity.bind(self));
 
 exports.cityRouter = router;
