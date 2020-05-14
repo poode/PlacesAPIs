f6868f743db4c15c6876bc507f6abcedc6b9d862
poode <eng.abdul.azeem@gmail.com>
2020-05-14 17:55:39 +0200

    adding upload folder and ignore contains and city api swagger

--- app/router/city.js
+++ app/router/city.js
@@ -8,7 +8,34 @@ const { addCitySchema } = require('../RequestSchemaList/addCitySchema');
 
 const { self, addCity } = require('../controllers/City');
 
-router.post('/', jwt, adminRole, validate(addCitySchema), addCity.bind(self));
-router.post('/users', jwt()/*, adminRole*/, validate(addCitySchema), addCity.bind(self));
+
+/**
+ * @swagger
+ * /cities:
+ *   post:
+ *     tags:
+ *       - Cities APIs
+ *     description: Any user can add city
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: x-auth-token
+ *         description: Token to sent in any request to identify the logged in User
+ *         in: header
+ *         required: true
+ *       - name: body
+ *         in: body
+ *         schema:
+ *            type: object
+ *            properties:
+ *               name:
+ *                  description: City name
+ *                  required: true
+ *                  type: string
+ *     responses:
+ *       200:
+ *         description: Returns created City
+ */
+router.post('/', jwt(), validate(addCitySchema), addCity.bind(self));
 
 exports.cityRouter = router;
